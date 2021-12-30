dd1885d74d0183655856258fd846ff6fc89ed48e
CJBNBG <70857041+CJBNBG@users.noreply.github.com>
2021-12-30 11:56:29 +0100

    Update entriestablepage.dart
    Einträge sind jetzt slidable nach rechts und links

--- lib/pages/EntriesTablePage/entriestablepage.dart
+++ lib/pages/EntriesTablePage/entriestablepage.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:sysdiapulsgew/services/dbhelper.dart';
 import '../../my-globals.dart' as globals;
 
-const EntryWidthSysDia = 45.0;
+const EntryWidthSysDia = 55.0;
 
 class EntriesTablePage extends StatefulWidget {
   const EntriesTablePage({Key? key}) : super(key: key);
@@ -20,7 +21,17 @@ class _EntriesTablePageState extends State<EntriesTablePage> {
   int _LimitFromSettings = 25;
   List<Map<String, dynamic>>_alleEintraege = [];
 
-  void _editItem(int ndx, int id) async {
+  void _editItem(int ndx) async {
+    int id = _alleEintraege[ndx]['pid'];
+    if ( id == null ) {
+      id = -1;
+    }
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text("noch zu programmieren... ($ndx/$id)"),
+            backgroundColor: Color.fromARGB(0xff, 0xbd, 0xbd, 0xbd)
+        )
+    );
     globals.updAVG_needed = true;
     print("editiere (index: $ndx - ID: $id)");
   }
@@ -30,8 +41,6 @@ class _EntriesTablePageState extends State<EntriesTablePage> {
       _LimitFromSettings = await dbHelper.getTabEntryCount();
       _alleEintraege = await dbHelper.getDataItems(_Limit);
       if ( _LimitFromSettings > _alleEintraege.length ) _LimitFromSettings = _alleEintraege.length;
-      print(_alleEintraege);
-      //_alleEintraege.forEach((element) {print(element.toString());});
     } on Error catch( _, e ) {
       print("keine Dateien gefunden");
     }
@@ -184,6 +193,10 @@ class _EntriesTablePageState extends State<EntriesTablePage> {
     _ladeDaten();
   }
 
+  void doNothing(BuildContext context) {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -238,15 +251,11 @@ class _EntriesTablePageState extends State<EntriesTablePage> {
                                 height: 40, width: EntryWidthSysDia,
                                 child: myListRowWidgetTwoLines(isHeader: true, Titel1: 'Gewicht', Titel2: '(kg)', Farbe1: Colors.grey, Farbe2: Colors.grey[500], Breite: EntryWidthSysDia, ScaleFactor: 0.75,),
                               ),
-                              Container(
-                                height: 40, width: 40,
-                                child: Text(""),
-                              ),
                             ],
                           ),
                         );
                       },
-                        childCount: 1,
+                        childCount: 1,                      // nur eine Überschriftenzeile
                       ),
                     ),
 
@@ -255,91 +264,101 @@ class _EntriesTablePageState extends State<EntriesTablePage> {
                     SliverFixedExtentList(
                       itemExtent: 60.0,
                       delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
-                        return Container(
-                          //height: 100,
-                          alignment: Alignment.center,
-                          //color: (index % 2) == 0 ? Colors.grey[200] : Colors.white,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                        return Slidable(
+                          startActionPane: ActionPane(
+                            // A motion is a widget used to control how the pane animates.
+                            motion: const ScrollMotion(),
+                            // All actions are defined in the children parameter.
                             children: [
-                              Container(
-                                height: 40, width: 80,
-                                child: myListRowWidgetTwoLines(
-                                  isHeader: false,
-                                  Titel1: dasDatum(_alleEintraege[index]['Zeitpunkt'].toString()),
-                                  Titel2: dieUhrzeit(_alleEintraege[index]['Zeitpunkt'].toString()),
-                                  Farbe1: globals.BgColorNeutral,
-                                  Farbe2: globals.BgColorNeutral,
-                                  Breite: 80,
-                                  ScaleFactor: 1.0,),
-                              ),
-                              Container(
-                                height: 40, width: EntryWidthSysDia,
-                                child: myListRowWidgetOneLine(
-                                  isHeader: false,
-                                  Titel1: _alleEintraege[index]['Systole'].toString(),
-                                  Farbe1: Farbe1Systole(_alleEintraege[index]['Systole']),
-                                  Farbe2: Farbe2Systole(_alleEintraege[index]['Systole']),
-                                  Breite: EntryWidthSysDia,
-                                  ScaleFactor: 1.0,),
-                              ),
-                              Container(
-                                height: 40, width: EntryWidthSysDia,
-                                child: myListRowWidgetOneLine(
-                                  isHeader: false,
-                                  Titel1: _alleEintraege[index]['Diastole'].toString(),
-                                  Farbe1: Farbe1Diastole(_alleEintraege[index]['Diastole']),
-                                  Farbe2: Farbe2Diastole(_alleEintraege[index]['Diastole']),
-                                  Breite: EntryWidthSysDia,
-                                  ScaleFactor: 1.0,),
-                              ),
-                              Container(
-                                height: 40, width: EntryWidthSysDia,
-                                child: myListRowWidgetOneLine(
-                                  isHeader: false,
-                                  Titel1: _alleEintraege[index]['Puls'].toString().isNotEmpty ? _alleEintraege[index]['Puls'].toString() : "---",
-                                  Farbe1: _alleEintraege[index]['Puls'].toString().isNotEmpty ? Farbe1Puls(_alleEintraege[index]['Puls']) : globals.BgColorNeutral,
-                                  Farbe2: _alleEintraege[index]['Puls'].toString().isNotEmpty ? Farbe2Puls(_alleEintraege[index]['Puls']) : globals.BgColorNeutral,
-                                  Breite: EntryWidthSysDia,
-                                  ScaleFactor: 1.0,),
-                              ),
-                              // Container(
-                              //   height: 40, width: EntryWidthSysDia,
-                              //   child: Text(_alleEintraege[index]['Gewicht'].toString()),
-                              // ),
-                              Container(
-                                height: 40, width: EntryWidthSysDia,
-                                child: myListRowWidgetOneLine(
-                                  isHeader: false,
-                                  Titel1: _alleEintraege[index]['Gewicht'].toString().isNotEmpty && _alleEintraege[index]['Gewicht'] != null ? _alleEintraege[index]['Gewicht'].toString() : "---",
-                                  Farbe1: _alleEintraege[index]['Gewicht'].toString().isNotEmpty ? Farbe1Gewicht(_alleEintraege[index]['Gewicht'].toString()) : globals.BgColorNeutral,
-                                  Farbe2: _alleEintraege[index]['Gewicht'].toString().isNotEmpty ? Farbe2Gewicht(_alleEintraege[index]['Gewicht'].toString()) : globals.BgColorNeutral,
-                                  Breite: EntryWidthSysDia,
-                                  ScaleFactor: 1.0,),
-                              ),
-                              Container(
-                                height: 40, width: 40,
-                                child: IconButton(
-                                  onPressed: (){
-                                    int TheID = _alleEintraege[index]['pid'];
-                                    if ( TheID == null ) {
-                                      TheID = -1;
-                                    }
-                                    _editItem(index, TheID);
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                        content: Text('noch zu programmieren... (index: $index - ID: $TheID)'),
-                                        backgroundColor: Color.fromARGB(0xff, 0xbd, 0xbd, 0xbd)
-                                      )
-                                    );
-                                  },
-                                  icon: Icon(Icons.edit),
-                                  color: Colors.lightBlue,
-                                  padding: EdgeInsets.zero,
-                                ),
+                              // A SlidableAction can have an icon and/or a label.
+                              SlidableAction(
+                                onPressed: (context) => {
+                                  _editItem(index)
+                                },
+                                backgroundColor: Color(0xFF21B7CA),
+                                foregroundColor: globals.BgColorNeutral,
+                                icon: Icons.edit,
+                                label: 'editieren',
                               ),
                             ],
+                          ),
+                          endActionPane: ActionPane(
+                            // A motion is a widget used to control how the pane animates.
+                            motion: const ScrollMotion(),
+                            // All actions are defined in the children parameter.
+                            children: [
+                              SlidableAction(
+                                onPressed: (context) => {
+                                  doNothing(context)
+                                },
+                                backgroundColor: Color(0xFFFE4A49),
+                                foregroundColor: globals.BgColorNeutral,
+                                icon: Icons.delete,
+                                label: 'löschen',
+                              ),
+                            ],
+                          ),
+                          child: Container(
+                            //height: 100,
+                            alignment: Alignment.center,
+                            //color: (index % 2) == 0 ? Colors.grey[200] : Colors.white,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 40, width: 80,
+                                  child: myListRowWidgetTwoLines(
+                                    isHeader: false,
+                                    Titel1: dasDatum(_alleEintraege[index]['Zeitpunkt'].toString()),
+                                    Titel2: dieUhrzeit(_alleEintraege[index]['Zeitpunkt'].toString()),
+                                    Farbe1: globals.BgColorNeutral,
+                                    Farbe2: globals.BgColorNeutral,
+                                    Breite: 80,
+                                    ScaleFactor: 1.0,),
+                                ),
+                                Container(
+                                  height: 40, width: EntryWidthSysDia,
+                                  child: myListRowWidgetOneLine(
+                                    isHeader: false,
+                                    Titel1: _alleEintraege[index]['Systole'].toString(),
+                                    Farbe1: Farbe1Systole(_alleEintraege[index]['Systole']),
+                                    Farbe2: Farbe2Systole(_alleEintraege[index]['Systole']),
+                                    Breite: EntryWidthSysDia,
+                                    ScaleFactor: 1.0,),
+                                ),
+                                Container(
+                                  height: 40, width: EntryWidthSysDia,
+                                  child: myListRowWidgetOneLine(
+                                    isHeader: false,
+                                    Titel1: _alleEintraege[index]['Diastole'].toString(),
+                                    Farbe1: Farbe1Diastole(_alleEintraege[index]['Diastole']),
+                                    Farbe2: Farbe2Diastole(_alleEintraege[index]['Diastole']),
+                                    Breite: EntryWidthSysDia,
+                                    ScaleFactor: 1.0,),
+                                ),
+                                Container(
+                                  height: 40, width: EntryWidthSysDia,
+                                  child: myListRowWidgetOneLine(
+                                    isHeader: false,
+                                    Titel1: _alleEintraege[index]['Puls'].toString().isNotEmpty ? _alleEintraege[index]['Puls'].toString() : "---",
+                                    Farbe1: _alleEintraege[index]['Puls'].toString().isNotEmpty ? Farbe1Puls(_alleEintraege[index]['Puls']) : globals.BgColorNeutral,
+                                    Farbe2: _alleEintraege[index]['Puls'].toString().isNotEmpty ? Farbe2Puls(_alleEintraege[index]['Puls']) : globals.BgColorNeutral,
+                                    Breite: EntryWidthSysDia,
+                                    ScaleFactor: 1.0,),
+                                ),
+                                Container(
+                                  height: 40, width: EntryWidthSysDia,
+                                  child: myListRowWidgetOneLine(
+                                    isHeader: false,
+                                    Titel1: _alleEintraege[index]['Gewicht'].toString().isNotEmpty && _alleEintraege[index]['Gewicht'] != null ? _alleEintraege[index]['Gewicht'].toString() : "---",
+                                    Farbe1: _alleEintraege[index]['Gewicht'].toString().isNotEmpty ? Farbe1Gewicht(_alleEintraege[index]['Gewicht'].toString()) : globals.BgColorNeutral,
+                                    Farbe2: _alleEintraege[index]['Gewicht'].toString().isNotEmpty ? Farbe2Gewicht(_alleEintraege[index]['Gewicht'].toString()) : globals.BgColorNeutral,
+                                    Breite: EntryWidthSysDia,
+                                    ScaleFactor: 1.0,),
+                                ),
+                              ],
+                            ),
                           ),
                         );
                         },
